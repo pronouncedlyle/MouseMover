d02c1ecc496a079014a09a0445334cf3b51d74d1
Matt <pronouncedlyle@gmail.com>
2021-02-27 17:13:43 -0500

    Built out core functionality

--- MouseMover/TitlePage.Designer.cs
+++ MouseMover/TitlePage.Designer.cs
@@ -29,9 +29,11 @@ namespace MouseMover
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.begin = new System.Windows.Forms.Button();
             this.stop = new System.Windows.Forms.Button();
             this.title = new System.Windows.Forms.TextBox();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // begin
@@ -42,6 +44,7 @@ namespace MouseMover
             this.begin.TabIndex = 0;
             this.begin.Text = "Begin";
             this.begin.UseVisualStyleBackColor = true;
+            this.begin.Click += new System.EventHandler(this.begin_Click);
             // 
             // stop
             // 
@@ -51,6 +54,7 @@ namespace MouseMover
             this.stop.TabIndex = 1;
             this.stop.Text = "Stop";
             this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
             // 
             // title
             // 
@@ -61,7 +65,12 @@ namespace MouseMover
             this.title.Text = "Mouse Mover 2000";
             this.title.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // Main
+            // Timer1
+            // 
+            this.Timer1.Interval = 5000;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // TitlePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -69,7 +78,7 @@ namespace MouseMover
             this.Controls.Add(this.title);
             this.Controls.Add(this.stop);
             this.Controls.Add(this.begin);
-            this.Name = "Main";
+            this.Name = "TitlePage";
             this.Text = "Mouse Mover 2000";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -81,6 +90,7 @@ namespace MouseMover
         private System.Windows.Forms.Button begin;
         private System.Windows.Forms.Button stop;
         private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Timer Timer1;
     }
 }
 
--- MouseMover/TitlePage.cs
+++ MouseMover/TitlePage.cs
@@ -16,5 +16,41 @@ namespace MouseMover
         {
             InitializeComponent();
         }
+
+        //method to actually move the mouse
+        private void MoveCursorThere()
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
+            Cursor.Clip = new Rectangle(this.Location, this.Size);
+        }
+
+        //TODO method to move the mouse back
+
+
+        //Initialize Timer
+        private void InitializeTimer()
+        {
+            Timer1.Interval = (5 * 1000);
+            Timer1.Tick += new EventHandler(Timer1_Tick);
+        }
+
+        //action to occur when timer hits designated interval
+        private void Timer1_Tick(object Sender, EventArgs e)
+        {
+            MoveCursorThere();
+        }
+
+        //method to turn it on
+        private void begin_Click(object sender, EventArgs e)
+        {
+            Timer1.Start();
+        }
+
+        //method to turn it off
+        private void stop_Click(object sender, EventArgs e)
+        {
+            Timer1.Stop();
+        }
     }
 }
